fe23b018dc1a5e35c3008e46fbd417cc0cc54da8
Implosions <ImplosionsTA@gmail.com>
2017-10-25 16:58:36 -0400

    Create Tokens.java to store tokens/removed from Info.java

--- src/Bot.java
+++ src/Bot.java
@@ -3,7 +3,6 @@ import java.util.concurrent.TimeUnit;
 
 import bullybot.classfiles.EventHandler;
 
-import bullybot.classfiles.Info;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -13,10 +12,9 @@ public class Bot{
 	
 	public static void main(String[] args) {
 		try{
-			OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).writeTimeout(1, TimeUnit.MINUTES);
-			JDA jda = new JDABuilder(AccountType.BOT).setToken(Info.TOKEN).setHttpClientBuilder(httpBuilder).buildBlocking();
+			OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS).connectTimeout(60, TimeUnit.SECONDS).writeTimeout(60, TimeUnit.SECONDS);
+			JDA jda = new JDABuilder(AccountType.BOT).setToken(Tokens.TESTBOT).setHttpClientBuilder(httpBuilder).buildBlocking();
 			jda.setAutoReconnect(true);
-			//jda.getPresence().setGame(Game.of("Videogames"));
 			jda.addEventListener(new EventHandler(jda));
 		}catch(Exception ex){
 			ex.printStackTrace();
