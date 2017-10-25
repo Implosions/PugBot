705df0ef13599a83fa57636a19d6d1731b92e2a8
Implosions <ImplosionsTA@gmail.com>
2017-10-25 17:14:00 -0400

    Added comments

--- src/Bot.java
+++ src/Bot.java
@@ -8,6 +8,9 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import okhttp3.OkHttpClient;
 
+// Bot driver
+// TODO: Load HTTP timeout settings from property file
+
 public class Bot{
 	
 	public static void main(String[] args) {
