4bc610763c79edd4250f6c60844a59a30f4f389b
Implosions <ImplosionsTA@gmail.com>
2017-10-27 21:28:43 -0400

    CmdDel will throw an error if player is not in any queue or waitlist

--- src/core/commands/CmdDel.java
+++ src/core/commands/CmdDel.java
@@ -8,9 +8,9 @@ import core.exceptions.InvalidUseException;
 import core.util.Utils;
 import net.dv8tion.jda.core.entities.Member;
 
-public class CmdDel extends Command{
-	
-	public CmdDel(){
+public class CmdDel extends Command {
+
+	public CmdDel() {
 		this.helpMsg = Constants.DEL_HELP;
 		this.description = Constants.DEL_DESC;
 		this.name = Constants.DEL_NAME;
@@ -19,22 +19,27 @@ public class CmdDel extends Command{
 	@Override
 	public void execCommand(Server server, Member member, String[] args) {
 		QueueManager qm = server.getQueueManager();
-		try{
-			if(args.length == 0){
-				qm.deletePlayer(member.getUser());
-			}else{
-				for(String q : args){
-					try {
-						qm.deletePlayer(member.getUser(), Integer.valueOf(q));
-					} catch (NumberFormatException ex) {
-						qm.deletePlayer(member.getUser(), q);
+		try {
+			if (qm.isPlayerInQueue(member.getUser()) || qm.isPlayerWaiting(member.getUser())) {
+				if (args.length == 0) {
+					qm.deletePlayer(member.getUser());
+				} else {
+					for (String q : args) {
+						try {
+							qm.deletePlayer(member.getUser(), Integer.valueOf(q));
+						} catch (NumberFormatException ex) {
+							qm.deletePlayer(member.getUser(), q);
+						}
 					}
 				}
+			}else{
+				throw new InvalidUseException("You are not in any queue");
 			}
 			qm.updateTopic();
-			this.response = Utils.createMessage(String.format("%s deleted from queue", member.getEffectiveName()), qm.getHeader(), true);
+			this.response = Utils.createMessage(String.format("%s deleted from queue", member.getEffectiveName()),
+					qm.getHeader(), true);
 			System.out.println(successMsg);
-		}catch(DoesNotExistException | InvalidUseException ex){
+		} catch (DoesNotExistException | InvalidUseException ex) {
 			this.response = Utils.createMessage("Error!", ex.getMessage(), false);
 		}
 	}
