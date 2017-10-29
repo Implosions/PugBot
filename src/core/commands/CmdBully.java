2ac678b80c2ad8281d0251d828fe5a5c984917fb
Implosions <ImplosionsTA@gmail.com>
2017-10-29 19:02:33 -0400

    Now loads actionList from file

--- src/core/commands/CmdBully.java
+++ src/core/commands/CmdBully.java
@@ -1,7 +1,12 @@
 package core.commands;
 
 import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import core.Constants;
 import core.entities.Server;
@@ -15,23 +20,7 @@ import net.dv8tion.jda.core.entities.User;
 
 public class CmdBully extends Command {
 
-	private String[] actionList = {"They used to call them jumpolines til <@%s>'s momma bounced on one back in '63",
-			"<@%s> so poor the ducks throw bread at him", "<@%s>'s momma so fat she on both sides of the family",
-			"Ay <@%s> yo momma so fat, I took a picture of her last Christmas and it's still printin'",
-			"Ay <@%s> yo momma so fat, she try to enter a ugly contest and the judges be like \"uh sry no professionals\"",
-			"Ay <@%s> yo momma so dumb she climbed over a glass wall to see what was on the other side",
-			"Ay <@%s> yo momma stink so bad she try to take a bath and the water jumped out",
-			"<@%s> so ugly when his mom drop him off for school she got a fine for littering",
-			"<@%s> breath smell so bad his dentist will only treat him over da phone",
-			"<@%s> so ugly, when I took him to the zoo, the security guard thank me for bringin him back",
-			"<@%s> face look like somethin i drew with my left hand",
-			"<@%s> is about as useful as a screen door on a submarine",
-			"<@%s> head so fat he ran a race and came in first AND last",
-			"when i look at <@%s> i understand why some animals eat their young",
-			"<@%s> so ugly his imaginary friend used to play with other kids",
-			"<@%s>: \"Siri, why am I single?\" *Siri activates front camera*",
-			"<@%s> so ugly, his portraits hang themselves", "<@%s> so dumb it take him 2 hours to watch 60 minutes",
-			"<@%s> so fat, I swerved to miss him and my car ran outta gas" };
+	private List<String> actionList = getActionList();
 	
 	private Random random = new Random();
 	private HashMap<User, Long> cooldownCollection = new HashMap<User, Long>();
@@ -61,7 +50,7 @@ public class CmdBully extends Command {
 						if (u.getId().equals(Constants.OWNER_ID)) {
 							u = member.getUser();
 						}
-						this.response = Utils.createMessage(String.format(actionList[random.nextInt(actionList.length)], u.getId()));
+						this.response = Utils.createMessage(String.format(actionList.get(random.nextInt(actionList.size())), u.getId()));
 						// Put user in cooldownCollection
 						if(!member.getUser().getId().equals(Constants.OWNER_ID)){
 							cooldownCollection.put(member.getUser(), System.currentTimeMillis());
@@ -80,6 +69,25 @@ public class CmdBully extends Command {
 		} catch (BadArgumentsException | DoesNotExistException ex) {
 			this.response = Utils.createMessage("Error!", ex.getMessage(), false);
 		}
+	}
+	
+	private List<String> getActionList(){
+		List<String> bl = new ArrayList<String>();
+		if (new File("app_data/bullylist.txt").exists()) {
+			try {
+				Scanner reader = new Scanner(new FileInputStream("app_data/bullylist.txt"));
+
+				while (reader.hasNextLine()) {
+					bl.add(reader.nextLine());
+				}
+				
+				reader.close();
+				return bl;
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 }
