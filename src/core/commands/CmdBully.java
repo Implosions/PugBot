4b85752376263ae17bec4746c3f35c694433c81f
Implosions <ImplosionsTA@gmail.com>
2018-07-18 17:05:42 -0400

    Remove cooldown

--- src/core/commands/CmdBully.java
+++ src/core/commands/CmdBully.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import core.Constants;
@@ -21,10 +20,8 @@ import net.dv8tion.jda.core.entities.User;
 
 public class CmdBully extends Command {
 
-	private final Integer COOLDOWN_TIME_MINUTES = 5;
 	private List<String> actionList = getActionList();
 	private Random random = new Random();
-	private HashMap<User, Long> cooldownCollection = new HashMap<User, Long>();
 
 	public CmdBully() {
 		this.helpMsg = Constants.BULLY_HELP;
@@ -40,25 +37,9 @@ public class CmdBully extends Command {
 
 			if (m != null) {
 				User u = m.getUser();
-				// Check command invoker is not on cooldown
-				if (!cooldownCollection.containsKey(member.getUser()) || System.currentTimeMillis()
-						- (60000 * COOLDOWN_TIME_MINUTES) >= cooldownCollection.get(member.getUser())) {
-					if (u.getId().equals(Constants.OWNER_ID)) {
-						u = member.getUser();
-					}
-					// Create message with random string in actionList
-					this.response = Utils
-							.createMessage(String.format(actionList.get(random.nextInt(actionList.size())), u.getId()));
-					// Put user in cooldownCollection
-					if (!member.getUser().getId().equals(Constants.OWNER_ID)) {
-						cooldownCollection.put(member.getUser(), System.currentTimeMillis());
-					}
-				} else {
-					this.response = Utils.createMessage("Your bullying is on cooldown", 
-							String.format("Time remaining: %d Minutes", 
-									COOLDOWN_TIME_MINUTES -
-									((System.currentTimeMillis() - cooldownCollection.get(member.getUser())) / 60000)), false);
-				}
+				String action = actionList.get(random.nextInt(actionList.size()));
+				response = Utils.createMessage(String.format(action, u.getId()));
+				
 			} else {
 				throw new InvalidUseException("User does not exist");
 			}
