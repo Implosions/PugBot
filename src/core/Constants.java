8a8f6e8da4d5564671e32a234788032544523213
Implosions <ImplosionsTA@gmail.com>
2018-08-20 01:25:59 -0400

    Add emoji constants

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -141,4 +141,19 @@ public class Constants {
 	public final static String LEAVEGROUP_NAME = "leavegroup";
 	public final static String LEAVEGROUP_HELP = "!leavegroup <name>";
 	public final static String LEAVEGROUP_DESC = "Leaves the specified group and removes the associated role";
+	
+	public static class Emoji {
+		public final static String CHECKMARK = "\u2705";
+		public final static String PUNCH = "\uD83D\uDC4A";
+		public final static String RAISED_HAND = "\u270B";
+		public final static String V = "\u270C";
+		public final static String STOP_SIGN = "\uD83D\uDED1";
+		public static final String FORWARD_BUTTON = "\u25B6";
+		public static final String BACK_BUTTON = "\u25C0";
+		public static final String NUMBER_1 = "\u0031\u20E3";
+		public static final String NUMBER_2 = "\u0032\u20E3";
+		public static final String NUMBER_3 = "\u0033\u20E3";
+		public static final String NUMBER_4 = "\u0034\u20E3";
+		public static final String NUMBER_5 = "\u0035\u20E3";
+	}
 }
