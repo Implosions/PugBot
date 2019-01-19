03fe104fa28a8da699dc57d8548b856254a39dc9
Implosions <ImplosionsTA@gmail.com>
2019-01-19 13:47:13 -0500

    Update junit

--- src/test/java/pugbot/core/entities/settings/serversettings/SettingCommandPrefixTest.java
+++ src/test/java/pugbot/core/entities/settings/serversettings/SettingCommandPrefixTest.java
@@ -1,9 +1,10 @@
 package pugbot.core.entities.settings.serversettings;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import pugbot.core.exceptions.InvalidUseException;
 
@@ -11,24 +12,24 @@ public class SettingCommandPrefixTest {
 	
 	private SettingCommandPrefix setting;
 	
-	@Before
+	@BeforeEach
 	public void setup() {
 		setting = new SettingCommandPrefix(0, new String());
 	}
 	
-	@Test(expected = InvalidUseException.class)
+	@Test
 	public void setThrowsInvalidUseExceptionIfArgsLengthIsGreaterThanOne() {
 		String[] args = "test input".split(" ");
 		
-		setting.set(args);
+		assertThrows(InvalidUseException.class, () -> setting.set(args));
 	}
 	
-	@Test(expected = InvalidUseException.class)
+	@Test
 	public void setThrowsInvalidUseExceptionIfNewPrefixLengthIsGreaterThanThree() {
 		String prefix = "!!!!";
 		String[] args = new String[] { prefix };
 		
-		setting.set(args);
+		assertThrows(InvalidUseException.class, () -> setting.set(args));
 	}
 	
 	@Test
